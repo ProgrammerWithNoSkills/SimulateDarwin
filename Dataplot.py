ab4d011d156c1bbecceee7a85134f9c1aea10c37
ProgrammerWithNoSkills <kitjohn796@gmail.com>
2021-09-12 21:14:11 +0800

    updated dataplot to allow comparison

--- Dataplot.py
+++ Dataplot.py
@@ -13,8 +13,6 @@ sns.set(rc={'axes.facecolor':'slategrey',
 BCB_Blue = '#00F2FE'
 BCB_Green = '#02EF69'
 BCB_Red = '#FE0000'
-color_list = [BCB_Blue, BCB_Green, BCB_Red]
-plt.rcParams['axes.prop_cycle'] = plt.cycler(color=color_list)
 
 #load data
 file = open('Assets/Data/Day_5.json')
@@ -36,19 +34,23 @@ df = pd.DataFrame({ xAxis[2:]:xAxisData, yAxis[2:]:yAxisData})
 print(df.sort_values(by=yAxis[2:]))
 
 #graph options
-ax = sns.regplot(x=xAxisData, y=yAxisData, data=data,
-                fit_reg=True, ci=50, truncate=True,
-                label=xAxis[2:])
+ax1 = sns.regplot(x=xAxisData, y=yAxisData, data=data,
+                fit_reg=True, ci=95, truncate=True,
+                label=xAxis[2:], color=BCB_Blue)
 
-ax = sns.regplot(x=zAxisData, y=yAxisData, data=data,
-                fit_reg=True, ci=50, truncate=True,
-                marker="x", label=zAxis[2:])
+ax2 = ax1.twiny()
+
+ax2 = sns.regplot(x=zAxisData, y=yAxisData, data=data,
+                fit_reg=True, ci=95, truncate=True,
+                marker="x", label=zAxis[2:], color=BCB_Green)
+
 #legend
-ax.legend()
+ax1.legend(loc=4)
+ax2.legend(loc=1)
 
 #axis and title
-plt.xlabel(xAxis[2:], size=14, font='Verdana', c='black')
-plt.ylabel(yAxis[2:], size=14, font='Verdana', c='black')
+ax1.set_xlabel(xAxis[2:])
+ax2.set_xlabel(zAxis[2:])
 plt.title('Best attribues', size=16, font='Verdana',
           fontstyle='italic', weight='bold', c='black')
 
