49ba267f3909a67def03604affae82b3b37618f1
ProgrammerWithNoSkills <kitjohn796@gmail.com>
2021-09-12 17:03:17 +0800

    Update Dataplot.py

--- Dataplot.py
+++ Dataplot.py
@@ -24,12 +24,12 @@ data = pd.read_json(file)
 xAxis = 'm_geneticMoveSpeed'
 yAxis = 'm_curFitness'
 #for comparing
-#zAxis = 'm_geneticMass'
+zAxis = 'm_geneticSightRange'
 
 #takes data and puts in list
 xAxisData = [i[xAxis] for i in data['Day_5']]
 yAxisData = [i[yAxis] for i in data['Day_5']]
-#zAxisData = [i[zAxis] for i in data['Day_5']]
+zAxisData = [i[zAxis] for i in data['Day_5']]
 
 #prints to console for debug
 df = pd.DataFrame({ xAxis[2:]:xAxisData, yAxis[2:]:yAxisData})
@@ -37,14 +37,20 @@ print(df.sort_values(by=yAxis[2:]))
 
 #graph options
 ax = sns.regplot(x=xAxisData, y=yAxisData, data=data,
-                robust=True, x_jitter=0.1, y_jitter=0.1)
+                fit_reg=True, ci=50, truncate=True,
+                label=xAxis[2:])
+
+ax = sns.regplot(x=zAxisData, y=yAxisData, data=data,
+                fit_reg=True, ci=50, truncate=True,
+                marker="x", label=zAxis[2:])
+#legend
+ax.legend()
 
 #axis and title
 plt.xlabel(xAxis[2:], size=14, font='Verdana', c='black')
 plt.ylabel(yAxis[2:], size=14, font='Verdana', c='black')
-plt.title(xAxis[2:] + ' vs ' + yAxis[2:],
-          size=16, font='Verdana', fontstyle='italic', weight='bold', c='black')
-
+plt.title('Best attribues', size=16, font='Verdana',
+          fontstyle='italic', weight='bold', c='black')
 
 
 plt.show()
